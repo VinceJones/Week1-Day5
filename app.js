6962ca25d5554d47b73ffc68fe77c7fdaab72d98
Vince Jones <vincethebutcher@gmail.com>
2015-04-18 21:17:23 -0500

    DOM output Update

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ var i;
 function makeCreature() {
 	var Person = {
 		str : parseInt( document.getElementById('strength').elements[0].value ),
-		dex : document.getElementById('dexterity').elements[0].value,
-		con : document.getElementById('constitution').elements[0].value,
-		wis : document.getElementById('wisdom').elements[0].value,
-		int : document.getElementById('intelligence').elements[0].value,
-		cha : document.getElementById('charisma').elements[0].value
-		//creatureClass : document.getElementById('creatureClass').elements[0].value
+		dex : parseInt( document.getElementById('dexterity').elements[0].value ),
+		con : parseInt( document.getElementById('constitution').elements[0].value ),
+		wis : parseInt( document.getElementById('wisdom').elements[0].value ),
+		int : parseInt( document.getElementById('intelligence').elements[0].value ),
+		cha : parseInt( document.getElementById('charisma').elements[0].value ),
+
+		creatureClass : document.getElementById('creatureClass').addEventListener('checked')
+
 		}
 	people.push( Person );
 
@@ -27,7 +29,7 @@ function makeCreature() {
 	people.push( Person );
 	for (i = 0; i < people.length; i++) {
 		document.getElementById("displayCreature").innerHTML = "Strength: " + people[i].str + "<br>Dexterity: " + people[i].dex + "<br>Constitution: " + people[i].con + "<br>Wisdom: " + people[i].wis + "<br>Intelligence: " + people[i].int + "<br>Charisma: " +people[i].cha + "<br>Class: " + people[i].creatureClass;
-		console.log("Strength: " + people[i].str + ", Dexterity: " + people[i].dex + ", Constitution: " + people[i].con);
+		console.log("Strength: " + people[i].str + ", Dexterity: " + people[i].dex + ", Constitution: " + people[i].con + ", Wisdom: " + people[i].wis + ", Intelligence: " + people[i].int + ", Charisma: " +people[i].cha + ", Class: " + people[i].creatureClass);
 	}
 }
 
